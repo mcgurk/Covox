0f3ff40d1c277c91f79ecf0d21ca34a35f98f91c
Jarno Lehtinen <lehtinen@sci.fi>
2023-05-31 11:38:49 +0300

    Update covox_5.x_test.ino

--- covox_5.x_test.ino
+++ covox_5.x_test.ino
@@ -25,7 +25,7 @@ Component config -> FreeRTOS -> Tick rate: 1000 (default 100)
 #define uint32_t unsigned int
 #define int32_t int
 
-#define VOLUME 7//8 // 0 min, 8 max //why 8 makes crackling?! to both channels!?
+#define VOLUME 8 // 0 min, 8 max
 #define DEBUG
 
 #define D0 13 // white
